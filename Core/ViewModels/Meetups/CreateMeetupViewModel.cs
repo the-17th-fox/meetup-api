877ff691cf42b76c76d054ac285f2b32e379f903
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-02-17 19:57:47 +0300

    Added service and viewmodels

---
+++ Core/ViewModels/Meetups/CreateMeetupViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.ViewModels.Meetups
+{
+    public class CreateMeetupViewModel
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public DateTime StartsAt { get; set; }
+        public string Speaker { get; set; } = string.Empty;
+        public string MeetupManager { get; set; } = string.Empty;
+    }
+}
---
+++ Core/ViewModels/ViewModelsErrors.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.ViewModels
+{
+    public abstract class ViewModelsErrors
+    {
+        public const string OutOfRange = "Provided values is out of permitted borders.";
+    }
+}
