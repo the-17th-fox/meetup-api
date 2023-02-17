49bf07fd9a9ec44e60b3b8868a1f79dc0ccdc349
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-02-17 17:28:59 +0300

    Added context, created initial migration

---
+++ Infrastructure/Context/MeetupDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context
+{
+    public class MeetupDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+    {
+        public MeetupDbContext(DbContextOptions<MeetupDbContext> options) : base(options) {}
+
+        public DbSet<Event> Events => Set<Event>();
+
+    }
+}
---
+++ Web/Extensions/ServicesInjection.cs
@@ -0,0 +1,16 @@
+﻿using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Web.Extensions
+{
+    internal static class ServicesInjection
+    {
+        public static void AddInfrastructure(this IServiceCollection services, string dbConnString)
+        {
+            services.AddDbContext<MeetupDbContext>(opt =>
+            {
+                opt.UseSqlServer(dbConnString);
+            });
+        }
+    }
+}
