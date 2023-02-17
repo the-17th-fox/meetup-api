062cc23a70eff0c3e9eab52b71f0c5dabdfaa0c4
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-02-17 19:57:07 +0300

    Added repository

---
+++ Core/ViewModels/Pagination/PageParametersViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.ViewModels.Pagination
+{
+    public class PageParametersViewModel
+    {
+        [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = ViewModelsErrors.OutOfRange)]
+        public int PageNumber { get; set; } = 1;
+        [Range(minimum: 1, maximum: 50, ErrorMessage = ViewModelsErrors.OutOfRange)]
+        public int PageSize { get; set; } = 20;
+    }
+}
