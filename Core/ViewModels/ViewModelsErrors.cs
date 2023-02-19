18e5bf1bfee190f11cffe18d00c4b7e392f66425
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-02-19 15:10:26 +0300

    Added tokens-specific view models

--- Core/ViewModels/ViewModelsErrors.cs
+++ Core/ViewModels/ViewModelsErrors.cs
@@ -9,5 +9,6 @@ namespace Core.ViewModels
     public abstract class ViewModelsErrors
     {
         public const string OutOfRange = "Provided values is out of permitted borders.";
+        public const string RequiredNotProvided = "Required field wasn't provided.";
     }
 }
