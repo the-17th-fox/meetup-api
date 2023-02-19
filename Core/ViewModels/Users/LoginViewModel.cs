9848bee1f2f4acf6ebd6af7fda079e14f19a3281
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-02-19 15:27:32 +0300

    Added users service and endpoints

---
+++ Core/ViewModels/Users/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.ViewModels.Users
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = ViewModelsErrors.RequiredNotProvided)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = ViewModelsErrors.RequiredNotProvided)]
+        [EmailAddress(ErrorMessage = "Provided email is incorrect.")]
+        public string Email { get; set; } = string.Empty;
+    }
+}
