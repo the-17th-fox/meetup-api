2b932110f92fa8ff5a1795f391b5a0210eb16ec0
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-02-19 16:00:29 +0300

    Added global errors handler middleware

---
+++ Web/Middlewares/GlobalErrorsHandler.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+using System.Text.Json;
+using Core.Constants.CustomExceptions;
+
+namespace Web.Middlewares
+{
+    public class GlobalErrorsHandler
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalErrorsHandler(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception exception)
+            {
+                var response = context.Response;
+                response.ContentType = "application/json";
+
+                switch (exception)
+                {
+                    case NotFoundException:
+                        response.StatusCode = (int)HttpStatusCode.NotFound;
+                        break;
+
+                    case BadRequestException:
+                        response.StatusCode = (int)HttpStatusCode.BadRequest;
+                        break;
+
+                    case UnauthorizedException:
+                        response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                        break;
+
+                    default:
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        break;
+                }
+
+                var exceptionResponse = new
+                {
+                    exceptionType = exception.GetType().Name,
+                    statusCode = response.StatusCode,
+                    message = exception.Message,
+                };
+
+                var result = JsonSerializer.Serialize(exceptionResponse);
+                await response.WriteAsync(result);
+            }
+        }
+    }
+}
