7007c37c809479c492cf3577b3b3f262ebee10f7
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2023-02-19 15:05:18 +0300

    Added refresh token db model

---
+++ Core/Models/RefreshToken.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    [Index(nameof(UserId), IsUnique = true)]
+    public class RefreshToken
+    {
+        [Key]
+        public Guid Token { get; set; }
+        public DateTime ExpiresAt { get; set; }
+        public DateTime IssuedAt { get; set; } = DateTime.UtcNow;
+        public bool IsRevoked { get; set; } = false;
+
+        public Guid UserId { get; set; }
+        public User User { get; set; } = null!;
+
+        [NotMapped]
+        public bool IsExpired => DateTime.UtcNow >= ExpiresAt;
+        [NotMapped]
+        public bool IsActive => !(IsExpired || IsRevoked);
+
+        public RefreshToken(DateTime expiresAt, Guid userId)
+        {
+            ExpiresAt = expiresAt;
+            UserId = userId;
+        }
+    }
+}
